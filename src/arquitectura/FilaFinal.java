39745c8c6785ab32b5701dc984855f8666dd4b62
Gabriel Serrano <gabo09link@gmail.com>
2022-05-16 00:09:26 -0500

    Creando la clase FilaFinal

---
+++ src/arquitectura/FilaFinal.java
@@ -0,0 +1,33 @@
+package arquitectura;
+
+/**
+ *
+ * @author Gabriel
+ */
+public class FilaFinal {
+    
+    private byte valor;
+    private byte repeticion = 0;
+
+    public FilaFinal() {
+    }
+
+    public byte getValor() {
+        return valor;
+    }
+
+    public void setValor(byte valor) {
+        this.valor = valor;
+    }
+
+    public byte getRepeticion() {
+        return repeticion;
+    }
+
+    public void setRepeticion(byte repeticion) {
+        this.repeticion = repeticion;
+    }
+    
+    
+    
+}
